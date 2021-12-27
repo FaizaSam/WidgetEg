8b89017ed4cfedc787b3f56f4e93940693327600
faizza.mohammed <faizza.mohammed@gmail.com>
2021-12-27 12:40:51 +0530

    modified

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,6 +1,9 @@
-import 'question.dart';
+import 'package:widget_egs/question.dart';
+import 'package:widget_egs/result.dart';
+
+import './quiz.dart';
 import 'package:flutter/material.dart';
-import 'answer.dart';
+import 'result.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -10,6 +13,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var _questions = [
+    {
+      'questionText': 'What\'s your Favourite colour',
+      'answers': ['Black', 'Red', 'Blue', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favourite Animal',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Which is your Favourite Place',
+      'answers': ['Pkd', 'Bglr', 'Cbe', 'Delhi'],
+    }
+  ];
   int _questionIndex = 0;
   void _answerQuestion() {
     setState(() {
@@ -17,39 +34,24 @@ class _MyAppState extends State<MyApp> {
     });
 
     print(_questionIndex);
+    /*if (_questionIndex < question.length) {
+      print('WE have more questions');
+    }*/
   }
 
   @override
   Widget build(BuildContext context) {
-    var question = [
-      {
-        'questionText': 'What\'s your Favourite colour',
-        'answers': ['Black', 'Red', 'Blue', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favourite Animal',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'Which is your Favourite Place',
-        'answers': ['pkd', 'Bglr', 'cbe', 'Delhi'],
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Widget example App'),
-        ),
-        body: Column(
-          children: [
-            Question(question[_questionIndex]['questionText'] as String),
-            ...(question[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Widget example App'),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questions: _questions,
+                  questionIndex: _questionIndex)
+              : Result()),
     );
   }
 }
